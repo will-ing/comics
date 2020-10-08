82110cc9c953c93ac1efd6534a2fa9bb679be1a4
will-ing <will-ing@github.com>
2020-10-07 19:23:36 -0700

    initial commit

---
+++ pages/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </nav>
+        </div>
+    )
+}
